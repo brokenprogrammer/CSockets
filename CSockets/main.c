6fcf266b38390ac57bbfefc8b59a2c8cc348ea52
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-03-17 22:05:31 +0100

    Initial commit

---
+++ CSockets/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSockets
+//
+//  Created by Oskar Mendel on 3/17/16.
+//  Copyright © 2016 Oskar Mendel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
